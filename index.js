c4009df070b55f5c777eed38478dfbce2d9f7251
jeremysimmons <jsimmons@jeremysimmons.net>
2020-08-25 14:12:31 -0600

    1 file modified

--- index.js
+++ index.js
@@ -4,25 +4,63 @@ const appDiv = document.getElementById('app');
 const append = function(str) {
   appDiv.insertAdjacentHTML( 'beforeend', str );
 }
-
-var numbers = [1,2,3,4,5,6,7,8,9];
-
 const asString = (array) => {
-  return JSON.stringify(array);
-}
-
-const incrementByOne = (list) => {
-   return list.map(x => x + 1);
+  return JSON.stringify(array, null, 2);
 }
 const arraysEqual = (arr1, arr2) => {
   return  JSON.stringify(arr1)===JSON.stringify(arr2);
 }
 
-var expectedResults = [2,3,4,5,6,7,8,9,10];
-var actualResults = incrementByOne(numbers);
+var input = [
+	{
+		color: "red",
+		value: "#f00"
+	},
+	{
+		color: "green",
+		value: "#0f0"
+	},
+	{
+		color: "blue",
+		value: "#00f"
+	},
+	{
+		color: "cyan",
+		value: "#0ff"
+	},
+	{
+		color: "magenta",
+		value: "#f0f"
+	},
+	{
+		color: "yellow",
+		value: "#ff0"
+	},
+	{
+		color: "black",
+		value: "#000"
+	}
+];
+
+var expectedResults = [
+  "red",
+  "green",
+  "blue",
+  "cyan",
+	"magenta",
+	"yellow",
+	"black",
+];
+
+const flatten = (list) => {
+   return list.map(x => x.color);
+}
+var actualResults = flatten(input);
+
+append(`<h1>Map - Flattening Data</h1>`)
 
 append(`<strong>Numbers</strong>`)
-append(`<pre>${asString(numbers)}</pre>`)
+append(`<pre>${asString(input)}</pre>`)
 
 append(`<strong>Expected</strong>`)
 append(`<pre>${asString(expectedResults)}</pre>`)
